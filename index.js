ff7b54014a55471c3056aa59eca000272e66ed94
Kiiza Atwiine Stephen <stephenkiiza123@gmail.com>
2022-04-29 13:10:04 +0300

    Node Express MongoDB CRUD Application

---
+++ index.js
@@ -0,0 +1,35 @@
+// Requiring Modules.
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Getting configurations.
+dotenv.config({ path: 'config.env' });
+
+// Application Setup.
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// Application Middlewares.
+app.use(morgan('tiny')); // Log Requests.
+app.use(bodyParser.urlencoded({ extended: true })); // Parse requests to body-parser.
+
+// Setup View Engine.
+app.set('view engine', "ejs");
+// app.set('views', path.resolve(__dirname, ""))
+
+// Load Assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+
+// Test Route
+// app.get('/', (req, res) => { res.send('Server CRUD Application.') })
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+// Server Initialization.
+app.listen(PORT, () => { console.log(`Server Application Running at http://localhost:${PORT}`) })
